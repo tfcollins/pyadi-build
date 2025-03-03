93863fa38d05feb373906a0d4df926e24f3cdecb
Travis F. Collins <travis.collins@analog.com>
2025-03-03 13:08:41 -0700

    WIP
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- test1.py
+++ test1.py
@@ -6,7 +6,7 @@ import adibuild as build
 # enable logging to show info messages
 logging.basicConfig(level=logging.DEBUG)
 
-build_type = "Linux"
+build_type = "Linux_ADSY"
 
 b = build.Builder(name="build")
 
@@ -68,6 +68,72 @@ elif build_type == "Linux_ADSY":
     linux.gitrepo_https = "https://bitbucket.analog.com/scm/sdg/linux-apollo.git"
     linux.branch = "main"
     b.add_software(linux, vivado)
+
+    def update_profile(self):
+        print("Updating profile")
+        filename = "vu11p-vpx-apollo.dts"
+        # Find the file with glob recursively
+        import glob
+
+        dts_file = glob.glob(f"{self.parent.build_dir}/linux/**/{filename}", recursive=True)
+        if not dts_file:
+            raise FileNotFoundError(f"Could not find {filename}")
+        
+        with open(dts_file[0], "r") as f:
+            lines = f.readlines()
+
+        def replace_line(target, new_line, lines):
+            out_lines = []
+            for line in lines:
+                if target in line:
+                    out_lines.append(new_line)
+                else:
+                    out_lines.append(line)
+            return out_lines
+        
+        def replace_line_after_found(target, new_line, lines):
+            out_lines = []
+            found = False
+            for line in lines:
+                if found:
+                    out_lines.append(new_line)
+                    found = False
+                else:
+                    out_lines.append(line)
+                if target in line:
+                    found = True
+            return out_lines
+        
+        # Apollo profile
+        ref = "adi,device-profile-fw-name"
+        lines = replace_line(ref, f'{ref} = "id01_uc26_ffsom.bin";\n', lines)
+
+        # ADF4382
+        ref = "adi,power-up-frequency"
+        lines = replace_line(ref, f'{ref} = /bits/ 64 <8000000000>;\n', lines)
+
+        # LTC6952
+        ref = "adi,vco-frequency-hz"
+        lines = replace_line(ref, f'{ref} = /bits/ 64 <2500000000>;\n', lines)
+
+        ref = "adi,extended-name = \"VUP Core CLK\";"
+        new = "adi,divider = <20>;\n"
+        lines = replace_line_after_found(ref, new, lines)
+
+        ref = "adi,extended-name = \"JESD REF CLK 1\";"
+        new = "adi,divider = <10>;\n"
+        lines = replace_line_after_found(ref, new, lines)
+
+        ref = "adi,extended-name = \"JESD REF CLK 2\";"
+        new = "adi,divider = <10>;\n"
+        lines = replace_line_after_found(ref, new, lines)
+
+        print("Writing to file")
+        with open(dts_file[0], "w") as f:
+            f.writelines(lines)
+
+
+    b.software[0].pre_build_func = update_profile
 
     a,c = b.build()
     print(a)
